d3d00cbdbe72cf4ecf96f11b73fb9fb35c2cc250
satya6408 <satyjeetzende6408@gmail.com>
2025-05-20 20:41:39 +0530

    average.java

---
+++ average.java
@@ -0,0 +1,11 @@
+class average
+{
+	public static void main(String[] args)
+	{
+	int sum,avg;
+	sum=40+50+60+68+78+80+45+90+88;
+	avg=sum/9;
+	System.out.println("sum="+sum);
+	System.out.println("average="+avg);
+	}
+}
